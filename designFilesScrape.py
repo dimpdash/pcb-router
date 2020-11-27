5e7de72f52f592ddf3190471c7fae6ecb4d48ffe
Ubuntu <azureuser@General-Virtual-Machine.4lkw0vypkcyergx0zvve3afc3h.bx.internal.cloudapp.net>
2020-11-27 23:46:41 +0000

    file scrape accidentally too short

--- designFilesScrape.py
+++ designFilesScrape.py
@@ -2,27 +2,17 @@ from scraperTools import getFileData, chunks
 import pickle
 from multiprocessing.pool import ThreadPool
 
-
- 
-with open("ids 23000.data", "rb") as fileHandle:
+with open("ids 23819.data", "rb") as fileHandle:
     fileIds = pickle.load(fileHandle)
 
-rawFiles =[]
-count=100
-batches = chunks(fileIds, 10)
-
-
-with open("raw files tmp 1", "rb") fileHandle:
-    rawFiles = pickle.load(fileHandle)
-
-
+batches = chunks(fileIds[7040:], 5000)
 count = 1
 fileCount = 1
 for batch in batches: 
     rawFiles = []
     with ThreadPool(10) as pool:
-        for rawFile in pool.map(getFileData, batch)
-            rawFiles = rawFiles.append(rawFile)
+        for rawFile in pool.map(getFileData, batch):
+            rawFiles.append(rawFile)
             print(fileCount)
             fileCount += 1
 
--- test.py
+++ test.py
@@ -1,11 +1,21 @@
 import pickle
+rawFiles = []
+for i in range(1,704):
+    print(i)
+    with open("raw files " + str(i) + ".data", "rb") as fileHandle:
+        rawFiles = rawFiles + pickle.load(fileHandle)
 
-with open("ids 271.data", "rb") as filehandle:
-    a = pickle.load(filehandle)
-    print(len(a))
+with open("raw files 0.data", "wb") as fileHandle:
+    pickle.dump(rawFiles,fileHandle)
 
 
-with open("ids 23000.data", "rb") as filehandle:
-    a = pickle.load(filehandle)
-    print(len(a))
+
+# with open("raw files 704.data", "rb") as filehandle:
+#     a = pickle.load(filehandle)
+#     print(a[1][0:100])
+
+
+# with open("ids 23819.data", "rb") as filehandle:
+#     a = pickle.load(filehandle)
+#     print(len(a))
 
